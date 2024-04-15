406306452a7d92941270481dd06c0d485a4fcc06
AshwinSakhare <asakhare@zdatainc.com>
2024-04-15 13:37:39 -0400

    remove weights; add setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+# Package metadata
+NAME = 'trans_path_enfuse'
+VERSION = '1.0.5'
+DESCRIPTION = 'Enfuse implementation of TransPath model from AIRI'
+AUTHOR = 'Enfuse.io'
+EMAIL = 'asakhare@zdatainc.com'
+URL = 'https://github.com/enfuse/transpath'
+LICENSE = 'MIT'
+PYTHON_VERSION = '>=3.11.0'
+
+# Long description from README.md
+with open('README.md', 'r', encoding = 'utf-8') as f:
+    long_description = 'you do not need the whole README here'
+
+# Required packages
+with open('requirements.txt', 'r') as f:
+    required_packages = f.read().splitlines()
+
+setup(
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    author = AUTHOR,
+    author_email = EMAIL,
+    url = URL,
+    license = LICENSE,
+    python_requires = PYTHON_VERSION,
+    packages = find_packages(),
+    package_data = {
+        'transpath': ['models/weights/*.pth']
+    },
+    install_requires = required_packages,
+    classifiers = [
+        'Development Status :: 4 - Production/Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.11',
+        'Operating System :: OS Independent'
+    ],
+)
